01c29043ca8ed79e585a157ae545f8d9da935cec
PASHAEV NASREDIN <151615853+miza1911@users.noreply.github.com>
2024-01-16 11:32:38 +0900

    new commit

---
+++ lesson-01.js
@@ -0,0 +1,2 @@
+let number1 = Math.floor(Math.random() * 100) +1
+console.log(number1);
--- lesson-01/02.js
+++
@@ -1,6 +0,0 @@
-let vol = 100
-
-let number1 = Math.floor(Math.random() * vol);
-
-number1 = Math.floor(number1);
-console.log(number1);
