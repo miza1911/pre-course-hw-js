d89d9676f39290f51d0f6be639d08d4dce55a0d4
PASHAEV NASREDIN <151615853+miza1911@users.noreply.github.com>
2024-01-19 03:34:13 +0900

    commit

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,15 +1,7 @@
-let number1 = Math.ceil(Math.random() * 100);
+let number1 = Math.floor(Math.random() * 100);
 
-console.log(number1);
-let gnum = prompt('вставь сюда полученное число');
-
-if (number1 == gnum) {
-console.log('Верно');
+if (number1 % 2 === 0) {
+    console.log(number1 + " :Это четное число");
 } else {
-console.log('Не верно, это было число ' + number1);
-}
-  {
-    if (number1 % 2 == 0) {
-        alert( number1 );
-    }
+    console.log(number1 + " :Это нечетное число");
 }
