e0a8fcdbf3b2831271701765660b09046010797d
PASHAEV NASREDIN <151615853+miza1911@users.noreply.github.com>
2024-02-09 17:23:46 +0900

    new commit

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,3 +1,3 @@
-let str = "zzzzyzz";
-let result = str.includes ("у") ? "нашел" : "заблудился";
-console.log(result);
+let str = 'zzzzyzz';
+let message = str.includes('y') ? 'нашел' : 'заблудился';
+console.log(message);
